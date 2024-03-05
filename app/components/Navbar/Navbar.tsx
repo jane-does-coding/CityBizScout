852e0cc25ef9fd5024cabc87b954cbd2d8ab98ab
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-03-05 16:30:39 -0700

    Authentication done and work: SignUp, Login, Logout

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import Container from "../Container";
+import Logo from "./Logo";
+import UserMenu from "./UserMenu";
+import { User } from "@prisma/client";
+
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
+  return (
+    <nav className="fixed w-full bg-white z-10">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            {currentUser?.name}
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/components/Navbar/UserMenu.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+
+const UserMenu = ({ currentUser }: { currentUser?: User | null }) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  return (
+    <div className="relative text-black">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="p-4 md:py-3 md:px-3 transition flex flex-row  items-center gap-3 rounded-full cursor-pointer hover:bg-neutral-100"
+          onClick={toggleOpen}
+        >
+          <AiOutlineMenu size={24} />
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="absolute shadow-md rounded-xl border-[1px] border-neutral-200 w-[40vw] md:w-[20vw] bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            {currentUser ? (
+              <>
+                <MenuItem onClick={loginModal.onOpen} label="Home" />
+                <MenuItem onClick={registerModal.onOpen} label="Logout" />
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={loginModal.onOpen} label="Login" />
+                <MenuItem onClick={registerModal.onOpen} label="Signup" />
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
