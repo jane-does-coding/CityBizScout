a95efbed01dce43c68c572164b831559cd61813b
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-03-07 17:30:24 -0700

    ui done

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body className={`${inter.className} bg-slate-700`}>
         <ToasterProvider />
         <RegisterModal />
